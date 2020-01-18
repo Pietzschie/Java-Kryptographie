1c120882e591867bfdb0b19b59d3cc1d6686bd01
Jannik <jannik.pflieger@gmail.com>
2020-01-18 07:34:37 +0100

    Aufgabe1 Changes

--- src/Aufgabe1/Hashfunction.java
+++ src/Aufgabe1/Hashfunction.java
@@ -3,27 +3,29 @@ package Aufgabe1;
 import java.security.MessageDigest;
 
 public class Hashfunction {
-    String plaintext;
-    String hashtext = "";
+    String plainText;
+    String hashText = "";
 
-    public void createHash(String plaintext, int offset) throws Exception{
-        this.plaintext = plaintext;
+    public String createHash(String plainText, int offset) throws Exception{
+        this.plainText = plainText;
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.reset();
-        md.update(plaintext.getBytes());
-        hashtext = toHexString(md.digest(), offset);
+        md.update(plainText.getBytes());
+        hashText = toHexString(md.digest(), offset);
+        return hashText;
     }
 
-    public boolean verfiy(String hash){
-        return hash.equals(hashtext);
+    public boolean verfiy(String hash, String toHash){
+        String h1 = hash.replaceAll("\\s+", "");
+        String h2 = toHash.replaceAll("\\s+", "");
+        return h1.equals(h2);
     }
 
     public void dumpHash(){
-        if (hashtext.equals("")) System.out.println("Die Nachricht wurde noch nicht verschlüsselt!!");
+        if (hashText.equals("")) System.out.println("Die Nachricht wurde noch nicht verschlüsselt!!\n");
         else {
-            System.out.println("Nachricht: " + plaintext);
-            System.out.println("Verschlüsselt: " + hashtext);
-            System.out.println("");
+            System.out.println("Nachricht: " + plainText);
+            System.out.println("Hash: " + hashText + "\n");
         }
     }
 
